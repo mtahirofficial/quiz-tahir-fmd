0261ebf593df5935df87f1e0af9151c9af8a1cf1
mtahirofficial <hmtahirs1@gmail.com>
2020-12-02 09:19:12 +0500

    Dataset - Questions/Answers

---
+++ src/api/DataSet.js
@@ -0,0 +1,47 @@
+const DATA_SET = [
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    options: ["<scripting>", "<js>", "<script>", "<javascript>"],
+    correct: 2,
+  },
+  {
+    question: "The external JavaScript file must contain the <script> tag.",
+    options: ["True", "False"],
+    correct: 1,
+  },
+  {
+    question: "Where is the correct place to insert a JavaScript?",
+    options: [
+      "Both the <head> section and the <body> section are correct",
+      "The <head> section",
+      "The <body> section",
+    ],
+    correct: 0,
+  },
+  {
+    question: "What does HTML stand for?",
+    options: [
+      "Home Tool Markup Language",
+      "Hyper Text Markup Language",
+      "Hyperlinks and Text Markup Language",
+    ],
+    correct: 1,
+  },
+  {
+    question: "Who is making the Web standards?",
+    options: ["World Wide Web Consortium", "Mozilla", "Microsoft"],
+    correct: 0,
+  },
+  {
+    question: "Choose the correct HTML element for the largest heading:",
+    options: ["h6", "heading", "h1"],
+    correct: 2,
+  },
+  {
+    question: "What is the correct HTML element for inserting a line break?",
+    options: ["break", "br", "lb"],
+    correct: 1,
+  },
+];
+
+export default DATA_SET;
