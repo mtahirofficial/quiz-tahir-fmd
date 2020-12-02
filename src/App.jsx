41de628ceb5697eee3b8558f61c8d3d2caddd2aa
mtahirofficial <hmtahirs1@gmail.com>
2020-12-02 09:18:34 +0500

    Questionnaire Added.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import Questionnaire from './components/Questionnaire'
+const App = () => {
+  return (
+    <div className='App'>
+      <Questionnaire />
+    </div>
+  );
+}
+
+export default App;
